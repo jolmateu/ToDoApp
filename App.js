82c34b514e8025cc7773b28b11dbcec2906b6896
jolmateu <jolmateu@byui.edu>
2023-10-28 00:54:19 -0500

    Second Commit

--- App.js
+++ App.js
@@ -1,20 +1,132 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+const App = () => {
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  const handleAddTask = () => {
+    if (task) {
+      setTasks([...tasks, { text: task, completed: false }]);
+      setTask('');
+    }
+  };
+
+  const handleToggleTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index].completed = !updatedTasks[index].completed;
+    setTasks(updatedTasks);
+  };
+
+  const handleRemoveTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
+  const renderItem = ({ item, index }) => (
+    <View style={[styles.task, item.completed ? styles.completed : null]}>
+      <Text style={styles.taskText}>{item.text}</Text>
+      <TouchableOpacity onPress={() => handleToggleTask(index)}>
+        <Text style={styles.checkmark}>{item.completed ? '✔' : ''}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => handleRemoveTask(index)}>
+        <Text style={styles.remove}>✖</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>ToDo List</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="New Task"
+          value={task}
+          onChangeText={(text) => setTask(text)}
+        />
+        <TouchableOpacity style={styles.addButton} onPress={handleAddTask}>
+          <Text style={styles.addButtonText}>Add</Text>
+        </TouchableOpacity>
+      </View>
+      <FlatList
+        style={styles.list}
+        data={tasks}
+        renderItem={renderItem}
+        keyExtractor={(_, index) => index.toString()}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffbe0b',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    marginBottom: 20,
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    borderColor: 'goldenrod',
+    borderWidth: 1,
+    marginRight: 10,
+    paddingHorizontal: 10,
+  },
+  addButton: {
+    backgroundColor: 'goldenrod',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+  },
+  addButtonText: {
+    color: 'black',
+    fontWeight: 'bold',
+  },
+  list: {
+    width: '100%',
+  },
+  task: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderBottomColor: 'black',
+    borderBottomWidth: 2,
+    padding: 10,
+    width: '100%',
+  },
+  taskText: {
+    flex: 1,
+    fontSize: 18,
+  },
+  completed: {
+    textDecorationLine: 'line-through',
+    opacity: 0.6,
+  },
+  checkmark: {
+    fontSize: 18,
+    marginRight: 10,
+  },
+  remove: {
+    fontSize: 18,
+    color: 'crimson',
+  },
 });
+
+export default App;
